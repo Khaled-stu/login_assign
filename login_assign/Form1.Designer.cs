1680683e8d92f6206a050d36f8997e0b1c1d373c
pc <pc@DESKTOP-DIEFINS>
2024-11-28 23:33:44 +0300

    Add project files.

---
+++ login_assign/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace login_assign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btnlog = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.user = new System.Windows.Forms.Label();
+            this.passs = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btnlog
+            // 
+            this.Btnlog.Location = new System.Drawing.Point(55, 215);
+            this.Btnlog.Name = "Btnlog";
+            this.Btnlog.Size = new System.Drawing.Size(89, 41);
+            this.Btnlog.TabIndex = 0;
+            this.Btnlog.Text = "Login";
+            this.Btnlog.UseVisualStyleBackColor = true;
+            this.Btnlog.Click += new System.EventHandler(this.Btnlog_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.BtnExit.Location = new System.Drawing.Point(200, 215);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(103, 42);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // user
+            // 
+            this.user.AutoSize = true;
+            this.user.Location = new System.Drawing.Point(27, 38);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(53, 13);
+            this.user.TabIndex = 2;
+            this.user.Text = "username";
+            // 
+            // passs
+            // 
+            this.passs.AutoSize = true;
+            this.passs.Location = new System.Drawing.Point(28, 102);
+            this.passs.Name = "passs";
+            this.passs.Size = new System.Drawing.Size(52, 13);
+            this.passs.TabIndex = 3;
+            this.passs.Text = "password";
+            this.passs.Click += new System.EventHandler(this.passs_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.user);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.passs);
+            this.groupBox1.Location = new System.Drawing.Point(41, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 145);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.Btnlog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btnlog;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label user;
+        private System.Windows.Forms.Label passs;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ login_assign/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace login_assign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void passs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+
+        private void Btnlog_Click(object sender, EventArgs e)
+        {
+            string x = "Khaled";
+            string y = "111222333";
+            int attempts = 0;
+            int maxAttempt = 3;
+            bool logedin = false;
+            if (!logedin)
+            {
+                while (attempts <= maxAttempt)
+                {
+
+                    if (textBox1.Text == x && textBox2.Text == y)
+                    {
+                        MessageBox.Show("login succeed");
+                        Btnlog.Text = "logout";
+                        attempts=0;
+                        break;
+                    }
+
+                    else if (textBox1.Text != x && textBox2.Text != y)
+                    {
+                        MessageBox.Show("both username and password wrong");
+                        attempts++;
+                        return;
+                    }
+                    else if (textBox1.Text != x && textBox2.Text == y)
+                    {
+                        MessageBox.Show("username wrong");
+                        attempts++;
+                        return;
+                    }
+
+                    else if (textBox1.Text == x && textBox2.Text != y)
+                    {
+                        MessageBox.Show("password wrong");
+                        attempts++;
+                        return;
+                    }
+                    
+                    if (attempts >= maxAttempt)
+                    {
+                        MessageBox.Show("attempt expired");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+                    
+                
+            
+        
+    
+
